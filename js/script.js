37f1b098eeb87725b79c2ab2f7577af53856f655
sjtorres <sandrotorres.ti@gmail.com>
2022-12-06 04:56:39 -0300

    Criado funções para melhorar o JS

--- js/script.js
+++ js/script.js
@@ -1,41 +1,82 @@
-pizzaJson.map((item, index) => {
-  //console.log(item)
+// funcoes auxiliares ou uteis
+const seleciona = (elemento) => document.querySelector(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
 
-  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
-  //console.log(pizzaItem)
+const abrirModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    seleciona('.pizzaWindowArea').style.display = 'flex'
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.opacity = 1
+    }, 150)
+}
 
-  document.querySelector('.pizza-area').append(pizzaItem)
+const fecharModal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
 
-  // preencher os dados de cada pizza
-  pizzaItem.querySelector('.pizza-item--img img').src = item.img
-  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2]}`
-  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+const botoesFechar = () => {
+    /*
+    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
+        //document.querySelector('.pizzaWindowArea').style.display = 'none'
+        fecharModal()
+    })
+    document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
+        //document.querySelector('.pizzaWindowArea').style.display = 'none'
+        fecharModal()
+    })
+    */
+    // BOTOES FECHAR MODAL
+    selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fecharModal)
+    })
+}
 
-  // pizza clicada
-  pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log('Clicou na pizza')
+const preencheDadosDasPizzas = (pizzaItem, item) => {
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price[2]}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+}
 
-    // abri a janela modal
-    document.querySelector('.pizzaWindowArea').style.display = 'flex'
+const preencheDadosModal = (item) => {
+    // document.querySelector('.pizzaBig img').src = item.img
+    // document.querySelector('.pizzaInfo h1').innerHTML = item.name
+    // document.querySelector('.pizzaInfo--desc').innerHTML = item.description
+    // document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+    seleciona('.pizzaBig img').src = item.img
+    seleciona('.pizzaInfo h1').innerHTML = item.name
+    seleciona('.pizzaInfo--desc').innerHTML = item.description
+    seleciona('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price[2]}`
+}
 
-    // preenchimento dos dados
-    document.querySelector('.pizzaBig img').src = item.img
-    document.querySelector('.pizzaInfo h1').innerHTML = item.name
-    document.querySelector('.pizzaInfo--desc').innerHTML = item.description
-    document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price[2]}`
+// MAPEAR pizzaJson para gerar lista de pizzas
+pizzaJson.map((item, index ) => {
+    //console.log(item)
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+    //console.log(pizzaItem)
+    //document.querySelector('.pizza-area').append(pizzaItem)
+    seleciona('.pizza-area').append(pizzaItem)
 
-  })
+    // preencher os dados de cada pizza
+    preencheDadosDasPizzas(pizzaItem, item)
+    
+    // pizza clicada
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+        e.preventDefault()
+        console.log('Clicou na pizza')
 
-  // fecha a janela modal (cancela)
-  document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
-    document.querySelector('.pizzaWindowArea').style.display = 'none'
-  })
-  
-  // fecha a janela modal Mobile (voltar)
-  document.querySelector('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
-    document.querySelector('.pizzaWindowArea').style.display = 'none'
-  })
+        // abrir janela modal
+        //document.querySelector('.pizzaWindowArea').style.display = 'flex'
+        abrirModal()
 
-})
+        // preenchimento dos dados
+        preencheDadosModal(item)
+
+    })
+
+    botoesFechar()
+
+}) // fim do MAPEAR pizzaJson para gerar lista de pizzas
